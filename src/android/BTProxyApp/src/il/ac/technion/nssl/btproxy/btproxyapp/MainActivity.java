0aef8ab9b081bca61dbb5444f880da752248c8bd
btproxy <btproxyproject@gmail.com>
2015-08-17 16:35:11 +0300

    BT client works

--- src/android/BTProxyApp/src/il/ac/technion/nssl/btproxy/btproxyapp/MainActivity.java
+++ src/android/BTProxyApp/src/il/ac/technion/nssl/btproxy/btproxyapp/MainActivity.java
@@ -1,65 +1,220 @@
+package il.ac.technion.nssl.btproxy.dummyapp;
 
-package il.ac.technion.nssl.btproxy.btproxyapp;
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.net.InetAddress;
-import java.net.Socket;
-import java.net.UnknownHostException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.UUID;
+
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-
+import android.view.Menu;
+import android.view.MenuItem;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.widget.TextView;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 
 public class MainActivity extends Activity {
+	
+	// ======================================================
+	// Private variables
+	// ======================================================
+	TextView out;
+	private static final int REQUEST_ENABLE_BT = 1;
+	private BluetoothAdapter btAdapter = null;
+	private BluetoothSocket btSocket = null;
+	private OutputStream outStream = null;
+	private InputStream inStream = null;
+	 
+	// ------------------------------------------------------
+	// Well known SPP UUID
+	// ------------------------------------------------------
+	private static final UUID MY_UUID =
+			UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+	// ------------------------------------------------------
+	// BT Server MAC address
+	// ------------------------------------------------------
+	// private static String address = "00:11:67:8A:6D:49"; // BTProxy MAC
+	// private static String address = "C0:F8:DA:C6:41:AB";  // itay's laptop MAC
+	private static String address = "00:11:67:55:90:57";  // CSR bluetooth MAC
+	
 
-private Socket socket;
+	// ======================================================
+	// Methods
+	// ======================================================
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		out = (TextView) findViewById(R.id.out);
+		out.append("\nIn onCreate()...");
+		
+		btAdapter = BluetoothAdapter.getDefaultAdapter();
+		CheckBTState();
+	}
 
-private static final int SERVERPORT = 4020;
-private static final String SERVER_IP = "132.68.60.114";
+	
+	@Override
+	protected void onStart() {
+		super.onStart();
+		out.append("\nIn onStart()...");
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		out.append("\nIn onResume...\n...Attempting client connect...");
+		
+		// Set up a pointer to the remote node using it's address.
+	    BluetoothDevice device = btAdapter.getRemoteDevice(address);
+	    
+	    // Using SDP, find the service according to MY_UUID, and create a socket to server
+	    // (Service should be SPP)
+	    try {
+	      btSocket = device.createRfcommSocketToServiceRecord(MY_UUID);
+	    } catch (IOException e) {
+	      AlertBox("Fatal Error", "In onResume() and socket create failed: " + e.getMessage() + ".");
+	    }
+	    
+	    // Discovery is resource intensive.  Make sure it isn't going on
+	    // when you attempt to connect and pass your message.
+	    btAdapter.cancelDiscovery();
+	 
+	    // Establish the connection.  This will block until it connects.
+	    try {
+	      btSocket.connect();
+	      out.append("\nConnection established and data link opened...");
+	    } catch (IOException e) {
+	      try {
+	        btSocket.close();
+	      } catch (IOException e2) {
+	        AlertBox("Fatal Error", "In onResume() and unable to close socket during connection failure" + e2.getMessage() + ".");
+	      }
+	    }
+	 
+	    // Create a data stream so we can talk to server.
+	    out.append("\nSending message to server...");
+	 
+	    // Send string to server
+	    try {
+	      outStream = btSocket.getOutputStream();
+	    } catch (IOException e) {
+	      AlertBox("Fatal Error", "In onResume() and output stream creation failed:" + e.getMessage() + ".");
+	    }
+	 
+	    String message = "Hello from Android.\n";
+	    byte[] msgBuffer = message.getBytes();
+	    try {
+	      outStream.write(msgBuffer);
+	    } catch (IOException e) {
+	      String msg = "In onResume() and an exception occurred during write: " + e.getMessage();
+	      // msg = msg +  ".\n\nCheck that the SPP UUID: " + MY_UUID.toString() + " exists on server.\n\n";
+	       
+	      AlertBox("Fatal Error", msg);       
+	    }
+	    out.append("\nMessage sent");
+//	    // Receive data from server
+//	    try {
+//	    	inStream = btSocket.getInputStream();
+//	    } catch (IOException e) {
+//	    	AlertBox("Fatal Error", "In OnResume() and input stream creation failed:" + e.getMessage() + ".");
+//	    }
+//	    byte[] readBuffer = null;
+//	    try {
+//	    	int charsRead = 0;
+//	    	String readMsg = "";
+//	    	while ((charsRead = inStream.read(readBuffer)) != -1) {
+//	    		readMsg += new String(readBuffer).substring(0, charsRead);
+//	    	}
+//		    out.append("Got message from server : " + readMsg);
+//		} catch (IOException e) {
+//			AlertBox("Fatal Error", "Couldn't read data. got error : " + e.getMessage());       
+//		}
+	}
+	
+	@Override
+	protected void onPause() {
+		super.onPause();
+		out.append("\nIn onPause()...");
+		 
+	    if (outStream != null) {
+	      try {
+	        outStream.flush();
+	      } catch (IOException e) {
+	        AlertBox("Fatal Error", "In onPause() and failed to flush output stream: " + e.getMessage() + ".");
+	      }
+	    }
+	 
+	    try     {
+	      btSocket.close();
+	    } catch (IOException e2) {
+	      AlertBox("Fatal Error", "In onPause() and failed to close socket." + e2.getMessage() + ".");
+	    }
+	}
+	
+	
+	@Override
+	protected void onStop() {
+		super.onStop();
+		out.append("\n...In onStop()...");
+	}
+	
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		out.append("\n...In onDestroy()...");
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
-@Override
-public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-	setContentView(R.layout.main);     
-
-    new Thread(new ClientThread()).start();
-}
-public void onClick(View view) {
-    try {
-        EditText et = (EditText) findViewById(R.id.EditText01);
-       
-        String str = et.getText().toString();
-        PrintWriter out = new PrintWriter(new BufferedWriter(
-                new OutputStreamWriter(socket.getOutputStream())),
-                true);
-        out.println(str);
-    } catch (UnknownHostException e) {
-        e.printStackTrace();
-    } catch (IOException e) {
-        e.printStackTrace();
-    } catch (Exception e) {
-        e.printStackTrace();
-    }
-}
-class ClientThread implements Runnable {
-
-    @Override
-    public void run() {
-        try {
-            InetAddress serverAddr = InetAddress.getByName(SERVER_IP);
-            socket = new Socket(serverAddr, SERVERPORT);
-
-        } catch (UnknownHostException e1) {
-            e1.printStackTrace();
-
-        } catch (IOException e1) {
-            e1.printStackTrace();
-        }
-
-    }
-
-}
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	private void CheckBTState() {
+	    // Check for Bluetooth support and then check to make sure it is turned on
+	 
+	    // Emulator doesn't support Bluetooth and will return null
+	    if(btAdapter==null) { 
+	      AlertBox("Fatal Error", "Bluetooth Not supported. Aborting.");
+	    } else {
+	      if (btAdapter.isEnabled()) {
+	        out.append("\n...Bluetooth is enabled...");
+	      } else {
+	        //Prompt user to turn on Bluetooth
+	        Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+	        startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+	      }
+	    }
+	  }
+	   
+	  public void AlertBox( String title, String message ){
+	    new AlertDialog.Builder(this)
+	    .setTitle( title )
+	    .setMessage( message + " Press OK to exit." )
+	    .setPositiveButton("OK", new OnClickListener() {
+	        public void onClick(DialogInterface arg0, int arg1) {
+	          finish();
+	        }
+	    }).show();
+	  }
+	
 }
